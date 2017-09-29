f16cd63d6c3ea9f6b14f8f0bea8733b2f0d1dade
PrashantVaidyanathan <prash@bu.edu>
2017-09-28 20:21:10 -0400

    changed location of page redirect in splash page

--- pages/javascript/splash.js
+++ pages/javascript/splash.js
@@ -5,12 +5,13 @@ function getStarted() {
         type: "GET",
         success: function (response) {
             console.log(response);
+            window.location.href = "./pages/specify.html";
         },
         error: function () {
             console.log("ERROR!!");
         }
     });
 
-    window.location.href = "./pages/specify.html";
+    
 }
 

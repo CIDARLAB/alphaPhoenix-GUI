84d0e93a0c6ffe942f311e25d6ce99b2158a969d
kelkind <kate@s-t-a-r-corp.com>
2017-09-28 20:20:15 -0400

    added function to send performance, struct, parts to server

--- pages/javascript/splash.js
+++ pages/javascript/splash.js
@@ -5,12 +5,13 @@ function getStarted() {
         type: "GET",
         success: function (response) {
             console.log(response);
+            document.cookie = "job=job123;path=/"
         },
         error: function () {
             console.log("ERROR!!");
         }
     });
-
+    window.localStorage.setItem("job","job123");
     window.location.href = "./pages/specify.html";
 }
 

f39b6d56759881a328e5e51d821a5ef2eeafa57a
Greg Frasco <frascog@bu.edu>
2017-11-14 14:05:20 -0500

    Flip between specify tabs

--- public/js/specify.js
+++ public/js/specify.js
@@ -1,2 +1,21 @@
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/chrome");
+
+function setTab(index) {
+    switch(index) {
+        case 0:
+            $('#stlTab').addClass('is-active');
+            $('#stl').show();
+            $('#gridTab').removeClass('is-active');
+            $('#grid').hide();
+            break;
+        case 1:
+            $('#stlTab').removeClass('is-active');
+            $('#stl').hide();
+            $('#gridTab').addClass('is-active');
+            $('#grid').show();
+            break;
+    }
+    return true;
+}
+setTab(0);

5622d27a51327ef8b4d45b14844630e23f4bd7ab
Greg Frasco <frascog@bu.edu>
2017-11-14 09:53:55 -0500

    Add Font Awesome, Add Example Icons

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp');
 var hb = require('gulp-hb');
+var watch = require('gulp-watch');
 
 gulp.task('css', function() {
-    gulp.src(['node_modules/bulma/css/bulma.css'])
-        .pipe(gulp.dest('public/css/'))
+    gulp.src([
+        'node_modules/bulma/css/bulma.css',
+        'node_modules/font-awesome/css/font-awesome.css',
+    ]).pipe(gulp.dest('public/css/'))
+});
+
+gulp.task('fonts', function() {
+    gulp.src([
+        'node_modules/font-awesome/fonts/fontawesome-webfont.eot',
+        'node_modules/font-awesome/fonts/fontawesome-webfont.woff',
+    ]).pipe(gulp.dest('public/fonts/'))
 });
 
 gulp.task('templates', function () {
@@ -15,4 +25,8 @@ gulp.task('templates', function () {
         .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', [ 'css','templates' ]);
+gulp.task('watch',['templates'], function () {
+    gulp.watch('./src/partials/*.handlebars' , ['templates']);
+});
+
+gulp.task('default', [ 'css','fonts','templates','watch']);

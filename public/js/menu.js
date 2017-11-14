f85e85a41c770711f2a67b7838d1bcfe98079399
Greg Frasco <frascog@bu.edu>
2017-11-14 14:31:42 -0500

    add specify menu links

---
+++ public/js/menu.js
@@ -0,0 +1,33 @@
+function setMenuIndex(index) {
+    switch(index) {
+        case 0:
+            $('#performanceTab').addClass('is-active');
+            $('#performance').show();
+            $('#structuralTab').removeClass('is-active');
+            $('#structural').hide();
+            $('#libraryTab').removeClass('is-active');
+            $('#library').hide();
+            break;
+        case 1:
+            $('#performanceTab').removeClass('is-active');
+            $('#performance').hide();
+            $('#structuralTab').addClass('is-active');
+            $('#structural').show();
+            $('#libraryTab').removeClass('is-active');
+            $('#library').hide();
+            break;
+        case 2:
+            $('#performanceTab').removeClass('is-active');
+            $('#performance').hide();
+            $('#structuralTab').removeClass('is-active');
+            $('#structural').hide();
+            $('#libraryTab').addClass('is-active');
+            $('#library').show();
+            break;
+    }
+    return true;
+}
+
+$().ready(function() {
+    setMenuIndex(0);
+})

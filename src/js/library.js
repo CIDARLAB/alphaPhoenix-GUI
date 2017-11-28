0ae5946d1b2f7dc6126534a24a64bad228095944
Greg Frasco <frascog@bu.edu>
2017-11-28 13:38:54 -0500

    Get parts from library

--- src/js/library.js
+++ src/js/library.js
@@ -20,22 +20,26 @@ $().ready(function () {
                         value: value.uri,
                     }));
                 });
-                getParts(registry,response[0].uri, 0);
+                getParts(registry,response[0].uri);
             },
         });
     }
 
-    function getParts(registry,url,page) {
+    function getParts(registry,url) {
         const urlParts = registry.split('//');
         if(urlParts[0].includes('http')) {
             urlParts.shift();
         }
-        const offset = 50 * page;
-        url = ['http://',urlParts[0],'/remoteSearch/uri=',url,'&?offset=',offset,'&limit=50'].join('');
+        var encodedUrl = encodeURIComponent(url);
+        url = ['http://',urlParts[0],'/remoteSearch/collection%3D%3C',encodedUrl,'%3E&?offset=0&limit=1000'].join('');
         $.ajax({
             url: url,
             success: function(response) {
-                console.log(response);
+                $('#libraryTable').empty();
+                for(var part of response) {
+                    $('#libraryTable').append('<tr><th>'+ part.displayId +'</th><th>'+ part.name +'</th> <th>'+ part.description +'</th></tr>')
+                }
+                $('#libraryIcon').empty().addClass('icon').removeClass('has-text-danger').addClass('has-text-success').append('<i class="fa fa-check-square"></i>');
             },
             error: function (response) {
                 $('#libraryIcon').empty().addClass('icon').removeClass('has-text-success').addClass('has-text-danger').append('<i class="fa fa-warning"></i>');

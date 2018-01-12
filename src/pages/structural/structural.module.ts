788f36927fbc97e9e50b6bde4aa004e35075bdb6
Greg Frasco <frascog@bu.edu>
2018-01-12 11:15:07 -0500

    Added Specification Pages

---
+++ src/pages/structural/structural.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { StructuralPage } from './structural';
+
+@NgModule({
+  declarations: [
+    StructuralPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(StructuralPage),
+  ],
+})
+export class StructuralPageModule {}

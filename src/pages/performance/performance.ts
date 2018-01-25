65d5e38f0315d4e13ba34ffb8f5f67a9bf7f5abd
Greg Frasco <frascog@bu.edu>
2018-01-25 11:33:46 -0500

    Add Ace Editor to Performance

--- src/pages/performance/performance.ts
+++ src/pages/performance/performance.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 import { MenuProvider } from "../../providers/menu";
 
@@ -11,14 +11,23 @@ export class PerformancePage {
 
   public mode = 'STL';
   public menuItem;
+  @ViewChild('editor') editorEle: ElementRef;
+  public ace = window['ace'];
+  public editor;
 
   constructor(public navCtrl: NavController, private menu: MenuProvider) {
     this.menuItem = this.menu.getMenuItem('PerformancePage');
     this.menuItem.status = 'Complete'
   }
 
+  ionViewDidLoad() {
+    this.editor = this.ace.edit(this.editorEle.nativeElement);
+    this.editor.setTheme("ace/theme/chrome");
+    this.editor.setShowPrintMargin(false);
+  }
+
   sample() {
-    console.log(this.menu.menu)
+    this.editor.setValue("((G[0,100] in0 <= 4) && (G[0,100] in0 >= 0)) &&\n(((G[0,50] out0 >= 0)&&(G[0,50] out0 <= 25)) && ((G[50,100] out0 >= 25)&&(G[50,100] out0 <= 36)))")
   }
 
 }

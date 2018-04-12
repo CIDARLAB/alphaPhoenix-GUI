3ede367da967638f6c1f175151799bea11bd74cd
Greg Frasco <frascog@bu.edu>
2018-04-12 15:52:59 -0400

    Load tokens from local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import {MenuProvider} from "../providers/menu";
+import {HttpProvider} from "../providers/http";
 
 @Component({
   templateUrl: 'app.html'
@@ -10,14 +11,20 @@ import {MenuProvider} from "../providers/menu";
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = 'WelcomePage';
+  rootPage: any;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public menu: MenuProvider) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public menu: MenuProvider,
+              public http:HttpProvider) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      if(this.http.token) {
+        this.rootPage = 'ProjectsPage';
+      } else {
+        this.rootPage = 'WelcomePage';
+      }
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });

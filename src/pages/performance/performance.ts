417543775311f3bc05a49fcd58c2cee8513af315
Greg Frasco <frascog@wit.edu>
2018-04-19 08:16:17 -0400

    Able to communicate to parent from editor

--- src/pages/performance/performance.ts
+++ src/pages/performance/performance.ts
@@ -23,26 +23,8 @@ export class PerformancePage {
     this.menuItem = this.menu.getMenuItem('PerformancePage');
     this.height = this.platform.height()- 275;
     this.menuCtrl.enable(true);
-    this.bindEvent(window, 'message', function (e) {
-      console.log('parnet',e.data)
-    });
   }
 
-  // addEventListener support for IE8
-  bindEvent(element, eventName, eventHandler) {
-    if (element.addEventListener){
-      element.addEventListener(eventName, eventHandler, false);
-    } else if (element.attachEvent) {
-      element.attachEvent('on' + eventName, eventHandler);
-    }
-  }
-
-
-
-  ionViewDidEnter() {
-    this.editor = this.editorEle.nativeElement.contentWindow;
-    //this.editor.postMessage('Hello','*')
-  }
   /*
   ionViewDidLoad() {
     this.editor = this.ace.edit(this.editorEle.nativeElement);

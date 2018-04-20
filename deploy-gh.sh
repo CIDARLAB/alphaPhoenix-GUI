d55129551a01f2ea0390c2c6b8e13b3fda0e8855
Greg Frasco <frascog@bu.edu>
2018-04-20 13:51:46 -0400

    update build script

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -4,15 +4,21 @@ git branch -D gh-pages
 git push origin --delete gh-pages
 git checkout -b gh-pages
 npm install
-npm run ionic:build -- --prod --device
+ionic build browser --prod
 rm README.md
 rm -rf src
 rm node_modules
 cp -r www/* .
 rm -rf www
+rm .editorconfig
+rm .gitignore
+rm ionic.config.json
+rm package.json
+rm package-lock.json
+rm tsconfig.json
+rm tslint.json
 git add .
-git reset circle.yml
 git reset deploy-gh.sh
 git commit -m "Publishing to gh-pages"
 git push -u origin gh-pages
-git checkout ionic
+git checkout master
